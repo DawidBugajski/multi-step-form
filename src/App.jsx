bd2a433783203d47d4fedf8e083bcb7cad7d0388
Dawid <dawidbugajski1996@gmail.com>
2023-01-27 21:48:07 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <div className='text-6xl text-red-500 font-medium'>JEDEN</div>
+      <div className='text-6xl text-cyan-400'>DWA</div>
+      <div className='text-6xl text-green-600 font-bold'>TRZY</div>
+    </>
+  );
+}
