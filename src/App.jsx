b56999fa2b64efdb0536ee4ea095d3cb5de50d76
Dawid <dawidbugajski1996@gmail.com>
2023-01-27 21:51:49 +0100

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function App() {
   return (
     <>
-      <div className='text-6xl text-red-500 font-medium'>JEDEN</div>
-      <div className='text-6xl text-cyan-400'>DWA</div>
-      <div className='text-6xl text-green-600 font-bold'>TRZY</div>
+      <div className='text-6xl text-red-500 font-medium'>ONE</div>
+      <div className='text-6xl text-cyan-400'>TWO</div>
+      <div className='text-6xl text-green-600 font-bold'>THREE</div>
     </>
   );
 }
